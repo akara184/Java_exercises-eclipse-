2270ee568eb6b7fe212e8be0b4f0b1da347d267c
akara184 <davisuporterj@gmail.com>
2023-10-18 07:34:07 -0300

    trycatch

---
+++ TryCatchExercicio/src/model/exception/AmountException.java
@@ -0,0 +1,11 @@
+package model.exception;
+
+public class AmountException extends RuntimeException {
+
+	public AmountException(String msg) {
+		super(msg);
+	}
+	public AmountException() {
+	}
+	
+}
