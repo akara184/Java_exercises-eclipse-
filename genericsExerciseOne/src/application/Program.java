ccb05373ca20270fa8ca5db3a6ae7e3948ff2b78
akara184 <davisuporterj@gmail.com>
2023-12-31 02:02:15 -0300

    Doing some Generics exercises

---
+++ genericsExerciseOne/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Scanner;
+
+import service.PrintService;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService<String> ps = new PrintService<>();
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		
+		for (int i=0; i < n; i++) {
+			String value = sc.next();
+			ps.addValue(value);
+			
+		}
+		ps.print();
+		String x = ps.first();
+		System.out.println("First: " + x);
+		
+		sc.close();
+	}//
+
+}
